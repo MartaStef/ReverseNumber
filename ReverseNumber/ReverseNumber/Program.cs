a71ed7e69e41cebc8f21b6fcd7ff0b7b8db82bba
Marta Stefanska <marta.stefanska@outlook.es>
2024-06-12 19:52:16 +0200

    wersja 1.0

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Hello in program ReverseNumber!");
+Console.WriteLine();
+
+string number;
+while (true)
+{
+    Console.WriteLine($"Enter a number:");
+    number = Console.ReadLine();
+
+    if (IsDigitsOnly(number))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Invalid input {number}. Please enter a valid number.");
+    }
+}
+List<char> digits = new List<char>();
+foreach (char digit in number)
+{
+    digits.Add(digit);
+}
+
+digits.Reverse();
+
+string newNumber = new string(digits.ToArray());
+
+Console.WriteLine();
+Console.WriteLine($"Reversed number: {newNumber}");
+
+static bool IsDigitsOnly(string number)
+{
+    foreach (char digit in number)
+    {
+        if (digit < '0' || digit > '9')
+        {
+            return false;
+        }
+    }
+    return true;
+}
